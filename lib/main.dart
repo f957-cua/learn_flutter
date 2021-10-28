216e8251df0568c26fb22e41bdd7fdba06604d15
ivanfilipchyk <ivanfilip@iSAPSolutionsltd.onmicrosoft.com>
2021-10-28 22:12:00 +0300

    second_variant

--- lib/main.dart
+++ lib/main.dart
@@ -3,7 +3,36 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(CounterWidget());
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        backgroundColor: Colors.indigo,
+        appBar: AppBar(
+          title: Text('Counter'),
+          centerTitle: true,
+        ),
+        body: Center(
+          child: Container(
+            child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: <Widget>[
+                  Text('Tap "-" to decrement',
+                      style: TextStyle(color: Colors.red, fontSize: 16)),
+                  CounterWidget(),
+                  Text('Tap "+" to decrement',
+                      style: TextStyle(color: Colors.red, fontSize: 16)),
+                ]),
+          ),
+        ),
+      ),
+    );
+  }
 }
 
 class CounterWidget extends StatefulWidget {
@@ -25,51 +54,34 @@ class _CounterWidgetState extends State<CounterWidget> {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      home: Scaffold(
-        appBar: AppBar(
-          title: Text('Counter'),
-          centerTitle: true,
-        ),
-        body: Center(
-          child: Container(
-            child: Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                crossAxisAlignment: CrossAxisAlignment.center,
-                children: <Widget>[
-                  Text('Tap "-" to decrement',
-                      style: TextStyle(color: Colors.red, fontSize: 16)),
-                  Row(
-                    children: [
-                      IconButton(
-                          onPressed: () {
-                            setState(() {
-                              _count--;
-                            });
-                          },
-                          icon: Icon(Icons.remove)),
-                      Text(
-                        '${(_count)}',
-                        style: TextStyle(
-                          color: Colors.black,
-                          fontSize: 20,
-                        ),
-                      ),
-                      IconButton(
-                          onPressed: () {
-                            setState(() {
-                              _count++;
-                            });
-                          },
-                          icon: Icon(Icons.add_rounded)),
-                    ],
-                    mainAxisAlignment: MainAxisAlignment.center,
-                  ),
-                  Text('Tap "+" to decrement',
-                      style: TextStyle(color: Colors.red, fontSize: 16)),
-                ]),
+    return Container(
+      child: Row(
+        children: [
+          IconButton(
+              color: Colors.white,
+              onPressed: () {
+                setState(() {
+                  _count--;
+                });
+              },
+              icon: Icon(Icons.remove)),
+          Text(
+            '${(_count)}',
+            style: TextStyle(
+              color: Colors.black,
+              fontSize: 20,
+            ),
           ),
-        ),
+          IconButton(
+              color: Colors.white,
+              onPressed: () {
+                setState(() {
+                  _count++;
+                });
+              },
+              icon: Icon(Icons.add_rounded)),
+        ],
+        mainAxisAlignment: MainAxisAlignment.center,
       ),
     );
   }
