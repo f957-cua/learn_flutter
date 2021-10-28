0fda5038ef9931556aef0ded415cea0f1f030497
ivanfilipchyk <ivanfilip@iSAPSolutionsltd.onmicrosoft.com>
2021-10-28 14:52:19 +0300

    done

--- lib/main.dart
+++ lib/main.dart
@@ -1,93 +1,39 @@
 // ignore_for_file: prefer_const_constructors
 
-import 'dart:async';
-
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(MyFirstApp());
+  runApp(MyApp());
 }
 
-class MyFirstApp extends StatefulWidget {
-  @override
-  State<StatefulWidget> createState() {
-    //TODO: implement createState
-    return _MyFirstAppState();
-  }
-}
-
-class _MyFirstAppState extends State<MyFirstApp> {
-  bool _loading = false;
-  double _progressValue = 0.0;
-
-  @override
-  void initState() {
-    _loading = false;
-    _progressValue = 0.0;
-    super.initState();
-  }
-
+class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: Scaffold(
-        backgroundColor: Colors.indigo,
-        appBar: AppBar(
-          title: Text('My first App'),
-          centerTitle: true,
-        ),
-        body: Center(
-          child: Container(
-            padding: EdgeInsets.all(16),
-            child: _loading
-                ? Column(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: <Widget>[
-                      LinearProgressIndicator(value: _progressValue),
-                      Text(
-                        '${(_progressValue * 100).round()}%',
-                        style: TextStyle(
-                          color: Colors.white,
-                          fontSize: 20,
-                        ),
-                      ),
-                    ],
-                  )
-                : Text(
-                    'Press button to download',
-                    style: TextStyle(
-                      color: Colors.white,
-                      fontSize: 20,
-                    ),
-                  ),
-          ),
-        ),
-        floatingActionButton: FloatingActionButton(
-          onPressed: () {
-            setState(() {
-              _loading = !_loading;
-              _updateProgress();
-            });
-          },
-          child: Icon(Icons.cloud_download),
-        ),
+        theme: ThemeData(fontFamily: 'IndieFlower'),
+        home: Scaffold(
+      appBar: AppBar(
+        title: Text('Adding Assets'),
+        centerTitle: true,
       ),
-    );
-  }
-
-  void _updateProgress() {
-    const oneSec = Duration(seconds: 1);
-    Timer.periodic(oneSec, (Timer t) {
-      setState(() {
-        _progressValue += 0.2;
-
-        if (_progressValue >= 1.0) {
-          _loading = false;
-          t.cancel();
-          _progressValue = 0.0;
-          return;
-        }
-      });
-    });
+      body: Center(
+        child: Stack(fit: StackFit.expand, children: <Widget>[
+          Image.asset('assets/images/purchase_background.png'),
+          Image.asset('assets/icons/Cover.JPG'),
+          Positioned(
+            top: 16,
+            left: 115,
+            child: Text(
+              'My custom font',
+              style: TextStyle(
+                fontSize: 30,
+                color: Colors.white,
+                // fontFamily: 'IndieFlower',
+              ),
+            ),
+          )
+        ]),
+      ),
+    ));
   }
 }
