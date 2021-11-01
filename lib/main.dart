bc22225c6e47854bb60b4a7eba70bb874f9283da
ivanfilipchyk <ivanfilip@iSAPSolutionsltd.onmicrosoft.com>
2021-11-01 11:53:13 +0200

    done

--- lib/main.dart
+++ lib/main.dart
@@ -3,86 +3,19 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(MyApp());
-}
-
-class MyApp extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      home: Scaffold(
-        backgroundColor: Colors.indigo,
-        appBar: AppBar(
-          title: Text('Counter'),
-          centerTitle: true,
-        ),
-        body: Center(
-          child: Container(
-            child: Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                crossAxisAlignment: CrossAxisAlignment.center,
-                children: <Widget>[
-                  Text('Tap "-" to decrement',
-                      style: TextStyle(color: Colors.red, fontSize: 16)),
-                  CounterWidget(),
-                  Text('Tap "+" to decrement',
-                      style: TextStyle(color: Colors.red, fontSize: 16)),
-                ]),
-          ),
-        ),
+  runApp(MaterialApp(
+    home: Scaffold(
+      appBar: AppBar(
+        title: Text('my first app'),
+        centerTitle: true,
       ),
-    );
-  }
-}
-
-class CounterWidget extends StatefulWidget {
-  @override
-  State<StatefulWidget> createState() {
-    // TODO: implement createState
-    return _CounterWidgetState();
-  }
-}
-
-class _CounterWidgetState extends State<CounterWidget> {
-  int _count = 50;
-
-  @override
-  void initState() {
-    _count = 50;
-    super.initState();
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Container(
-      child: Row(
-        children: [
-          IconButton(
-              color: Colors.white,
-              onPressed: () {
-                setState(() {
-                  _count--;
-                });
-              },
-              icon: Icon(Icons.remove)),
-          Text(
-            '${(_count)}',
-            style: TextStyle(
-              color: Colors.black,
-              fontSize: 20,
-            ),
-          ),
-          IconButton(
-              color: Colors.white,
-              onPressed: () {
-                setState(() {
-                  _count++;
-                });
-              },
-              icon: Icon(Icons.add_rounded)),
-        ],
-        mainAxisAlignment: MainAxisAlignment.center,
+    body: Center(
+      child: Text('Hello ninjas'),
       ),
-    );
-  }
+      floatingActionButton: FloatingActionButton(
+        child: Text('click'),
+        onPressed: () => null,
+        ),
+    ),
+  ));
 }
